e1b2e2c6a4ee382577e0ac84cee5f87008d5b26e
LostX228 <abdekr40@gmail.com>
2024-09-25 05:17:45 +0100

    Create S.js

---
+++ S.js
@@ -0,0 +1,17 @@
+function lock(input) {//ghost
+    if (input.checked) {
+h5gg.clearResults();
+modifyValue(1077936128, -20001, 'A04');
+modifyValue(1069547520, 1148829696, 'A10');
+alert("AimLock ON");
+h5gg.clearResults();
+      codeStatus = true;
+    } else {     
+h5gg.clearResults();
+modifyValue(-20001, 1077936128, 'A04');
+modifyValue(1148829696, 1069547520, 'A10');
+alert("AimLock OFF");
+h5gg.clearResults();
+        codeStatus = false;
+    }
+}
