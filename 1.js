c96c66ebb0b7bdc721c1d4c6f0fb47d7ed552e44
LostX228 <abdekr40@gmail.com>
2024-09-25 05:04:57 +0100

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,20 @@
+
+
+
+ function aim180() {
+        h5gg.clearResults();
+        h5gg.searchNumber('1057048494','I32','0x100000000', '0x160000000');
+        h5gg.editAll('-2000', 'I32'); 
+        h5gg.clearResults();
+        h5gg.searchNumber('1054951342','I32','0x100000000', '0x160000000');
+        h5gg.editAll('-2000', 'I32'); 
+        h5gg.clearResults();
+        h5gg.searchNumber('1053273620','I32','0x100000000', '0x160000000');
+        h5gg.editAll('-2000', 'I32'); 
+        h5gg.clearResults();
+				
+				vant.showNotify({
+        type: 'success',
+        message: 'Aim 180'
+			});
+    }
