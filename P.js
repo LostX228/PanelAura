5ef1907d5f5118a374644bae9be63cffcf557e59
LostX228 <abdekr40@gmail.com>
2024-09-25 05:11:00 +0100

    Create P.js

---
+++ P.js
@@ -0,0 +1,9 @@
+function Aimlock() {//ghost
+    if (input.checked) {
+h5gg.clearResults();
+modifyValue(1077936128, -20001, 'A04');
+modifyValue(1069547520, 1148829696, 'A10');      	
+		vant.showNotify({
+        type: 'success',
+        message: 'Aim Lock'
+			});
