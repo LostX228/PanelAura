08e4c59c0090c8df7dd41b60dc0d9e52d36d1caf
LostX228 <abdekr40@gmail.com>
2024-09-25 03:45:20 +0100

    Create G.js

---
+++ G.js
@@ -0,0 +1,10 @@
+loopy() 
+function loopy() { 
+    var sWord = "LostX"; 
+    var username = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 "); // طلب اسم المستخدم
+    while (sWord != "1") {
+        sWord = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐓𝐡𝐞 𝐊𝐞𝐲 ");  
+    } 
+
+    window.alert(" ⪼  𝐖𝐚𝐥𝐜𝐨𝐦 𝐓𝐨 𝐏𝐚𝐧𝐞𝐥 𝐋𝐨𝐬𝐭𝐗  ");
+}
