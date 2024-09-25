6e08d8ce02f45023e081e4e68e2b85935258e00e
LostX228 <abdekr40@gmail.com>
2024-09-25 03:40:04 +0100

    Create XNX.js

---
+++ XNX.js
@@ -0,0 +1,9 @@
+  function loopy() { 
+        var sWord = "LostX"; 
+        var username = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 "); // طلب اسم المستخدم
+        while (sWord != "LostX") {
+            sWord = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐓𝐡𝐞 𝐊𝐞𝐲 ");  
+        } 
+    
+        window.alert(" ⪼  𝐖𝐚𝐥𝐜𝐨𝐦 𝐓𝐨 𝐏𝐚𝐧𝐞𝐥 𝐋𝐨𝐬𝐭𝐗  " + username);
+    }
