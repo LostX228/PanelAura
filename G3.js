88cea89bb9dadcb40ddb5780e91915fdbd4ac3bd
LostX228 <abdekr40@gmail.com>
2024-09-25 03:52:32 +0100

    Create G3.js

---
+++ G3.js
@@ -0,0 +1,11 @@
+function loopy() { 
+    var sWord = "LostX"; 
+    var username = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞: ");
+    var password = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐏𝐚𝐬𝐬𝐰𝐨𝐫𝐝: ");
+
+    while (sWord != "1") {
+        sWord = prompt(" ≫  𝐄𝐧𝐭𝐞𝐫 𝐓𝐡𝐞 𝐊𝐞𝐲 ");  
+    } 
+
+    window.alert(" ⪼  𝐖𝐚𝐥𝐜𝐨𝐦 𝐓𝐨 𝐏𝐚𝐧𝐞𝐥 𝐋𝐨𝐬𝐭𝐗  " + username);
+}
