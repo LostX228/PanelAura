7b99e51797cab609739f37f3f6d0a188497aac7b
LostX228 <abdekr40@gmail.com>
2024-09-28 09:22:13 +0100

    Create M9W.js

---
+++ M9W.js
@@ -0,0 +1,30 @@
+
+function T1() {
+    location.href = "https://t.me/hpntp";
+    vant.showNotify({
+        type: 'success',
+        message: 'Telegram'
+    });
+}
+function T2() {
+  location.href = "https://ig.me/j/AbZZAlTaWWC-HW_a/";
+  vant.showNotify({
+        type: 'success',
+        message: 'instgram'
+    });
+}
+function T3() {
+    location.href = "https://discord.gg/h7239bMk";
+    vant.showNotify({
+        type: 'success',
+        message: 'Discord'
+    });
+}
+function T4() {
+  location.href = "https://t.me/+nrMeH94pkItmMGJk";
+  vant.showNotify({
+        type: 'success',
+        message: 'Whatsapp'
+    });
+}
+
